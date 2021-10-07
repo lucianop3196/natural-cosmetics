434a091939da166ab9c35024109df87c59ff75af
Luciano Pennacchioni <luciano.p3196@gmail.com>
2021-10-06 23:30:02 -0300

    h1 added to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Proyecto de cosmética natural
-        </a>
+        <h1>Proyecto de cosmética natural</h1>
       </header>
     </div>
   );
