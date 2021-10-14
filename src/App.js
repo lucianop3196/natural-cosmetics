f19c20ee6a1b29230360561a44b9a3edf11b66c7
Luciano Pennacchioni <luciano.p3196@gmail.com>
2021-10-13 23:11:42 -0300

    Se eliminaron carpetas que venían por default + se creó el componente Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <h1>Proyecto de cosmética natural</h1>
       </header>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Navbar.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="/home">Home</a>
+      <a href="/about">About</a>
+      <a href="/shop">Shop</a>
+      <a href="/contact">Contact</a>
+      <a className="signIn" href="/signIn">Sign In</a>
+    </div>
+  );
+}
+
+export default Navbar;
