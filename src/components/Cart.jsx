82f7f26a7c773450b43e45a18f525b29a1a6a89e
Luciano <luciano.p3196@gmail.com>
2021-11-16 16:34:05 -0300

    Se mejoro el diseño estetico del componente Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,25 +1,59 @@
 import { useCartContext } from "../context/CartContext";
+import "../styles/Cart.css";
 
 function Cart() {
   const { cartList, deleteFromCart, clearItems } = useCartContext();
   let finalPrice = 0;
-  for (let i in cartList) { 
-    finalPrice += parseInt(cartList[i].props.price)*cartList[i].quantity;
+  for (let i in cartList) {
+    finalPrice += parseInt(cartList[i].props.price) * cartList[i].quantity;
   }
   return (
     <>
-      {cartList.map((item) => (
-        <div key={item.props.id}>
-          <div>{item.props.name}</div>
-          <div>${item.props.price}</div>
-          <div>Cantidad: {item.quantity}</div>
-          <button onClick={() => deleteFromCart(item.props.id)}>
-            Eliminar producto
-          </button>
-        </div>
-      ))}
-      <button onClick={() => clearItems()}>Vaciar carrito</button>
-      <div>Total: ${finalPrice} </div>
+      <div className="cartContainer">
+        <h2>Detalle de tu compra</h2>
+        <ul className="responsive-table">
+          <li className="table-header">
+            <div className="col col-1">Producto</div>
+            <div className="col col-2">Cantidad</div>
+            <div className="col col-3">Precio</div>
+            <div className="col col-4"></div>
+          </li>
+
+          {cartList.map((item) => (
+            <li key={item.props.id} className="table-row">
+              <div className="col col-1" data-label="name">
+                {item.props.name}
+              </div>
+              <div className="col col-2" data-label="quantity">
+                {item.quantity}
+              </div>
+              <div className="col col-3" data-label="price">
+                ${item.props.price}
+              </div>
+              <div className="btnDelete col col-4">
+                <button
+                  className="btnDelete"
+                  title="Eliminar item"
+                  onClick={() => deleteFromCart(item.props.id)}
+                >
+                  <i class="fas fa-minus-circle"></i>
+                </button>
+              </div>
+            </li>
+          ))}
+
+          <li className="table-row">
+            <button
+              className="btnClear"
+              title="Vaciar carrito"
+              onClick={() => clearItems()}
+            >
+              <i class="far fa-trash-alt"></i>
+            </button>
+            <div>Total: ${finalPrice}</div>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
